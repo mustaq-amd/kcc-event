42e915aece6c116d83a89c0d36557fff5be01767
mustaq-amd <97461845+mustaq-amd@users.noreply.github.com>
2023-10-13 12:05:55 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,71 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'kcc-event';
+
+  teamName: string = '';
+  teams: string[] = [];
+  resultsList: any[];
+
+  addTeam() {
+    if (this.teamName.trim() !== '' || this.teamName.trim() !== null) {
+      this.teams.push(this.teamName);
+      this.teamName = '';
+    }
+  }
+
+  generateMatches() {
+    const matches = [];
+    const teams = this.teams;
+
+    let matchnumber;
+
+    if (teams.length == 4) {
+      matchnumber = 2;
+    }
+
+    // Generate all possible combinations of matches
+    for (let i = 0; i < teams.length; i++) {
+      for (let j = i + 1; j < teams.length; j++) {
+        matches.push([teams[i], teams[j]]);
+      }
+    }
+
+    // Randomly shuffle the matches
+    for (let i = matches.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [matches[i], matches[j]] = [matches[j], matches[i]];
+    }
+
+    // Each team plays exactly 2 matches
+    const teamMatches = new Map();
+    const result = [];
+
+    for (const match of matches) {
+      const [team1, team2] = match;
+
+      if (!teamMatches.has(team1)) {
+        teamMatches.set(team1, 0);
+      }
+
+      if (!teamMatches.has(team2)) {
+        teamMatches.set(team2, 0);
+      }
+
+      if (
+        teamMatches.get(team1) < matchnumber &&
+        teamMatches.get(team2) < matchnumber
+      ) {
+        result.push(match);
+        console.log('result : ', result);
+        teamMatches.set(team1, teamMatches.get(team1) + 1);
+        teamMatches.set(team2, teamMatches.get(team2) + 1);
+      }
+    }
+
+    this.resultsList = result;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
